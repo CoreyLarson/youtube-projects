9f1279d08aac27195ac88efef22032dea3e258ff
CoreyLarson <corey.e.larson21@gmail.com>
2025-05-04 21:11:33 -0700

    IntroToPython

---
+++ introToPython/index.py
@@ -0,0 +1,9 @@
+#name = "M&O Coding" # "1 # S"
+age = 20 # 1, 2, 3, ... 100 -> -1, -2, -3, .. -100
+height = 5.10 #
+is_student = True #3 Apples, and 2 oranges -> Do I have the same amount of apples as oranges False, Do I have different amount of apples and oranges True
+gender = 'M' # ' 1, A a, #'
+
+
+name = input("What is your name? ")
+print("Nice to meet you, " + name + "!")
